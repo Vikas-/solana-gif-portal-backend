8ecba27eea3cf52401a3652e7955f89d67f3667d
Vikas- <vikasreddy.k11@gmail.com>
2021-11-09 08:35:37 +0530

    Updated

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -5,31 +5,73 @@ declare_id!("CB2fEzdEJhrUeh4Y67rfMYNKBLHr2d3TNE896V2TbnLq");
 #[program]
 pub mod myepicproject {
     use super::*;
-    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    pub fn new_list(ctx: Context<NewList>, bump: u8) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
+        base_account.bump = bump;
+        base_account.gifs = Vec::new();
         base_account.total_gifs = 0;
         Ok(())
     }
 
-    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+    pub fn add_gif(ctx: Context<AddGif>, url: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-
-        let item = ItemStruct {
-            gif_link: gif_link.to_string(),
-            user_address:  *base_account.to_account_info().key,
+        let item = GifItem {
+            url: url.to_string(),
+            adder:  *ctx.accounts.user.to_account_info().key,
+            votes: 0
         };
-
-        base_account.gif_list.push(item);
+        base_account.gifs.push(item);
         base_account.total_gifs += 1;
         Ok(())
     }
+
+    pub fn upvote_gif(ctx: Context<UpvoteGif>, url: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let item = base_account
+                    .gifs
+                    .iter_mut()
+                    .find(|item| item.url == url)
+                    .ok_or(Err::NoItemFound)?;
+        item.votes += 1;
+        Ok(())
+    }
+
+    pub fn send_sol(ctx: Context<SendSol>, amount: u64) -> ProgramResult {
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.from.key(),
+            &ctx.accounts.to.key(),
+            amount as u64
+        );
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.from.to_account_info(),
+                ctx.accounts.to.to_account_info(),
+            ]
+        )
+    }
+}
+
+#[derive(Accounts)]
+pub struct SendSol<'info> {
+    #[account(mut)]
+    from: Signer<'info>,
+    #[account(mut)]
+    to: AccountInfo<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[error]
+pub enum Err {
+    #[msg("No item with that url found")]
+    NoItemFound,
 }
 
 // Attach certain variable to StartStuffOff Account
 #[derive(Accounts)]
-pub struct StartStuffOff<'info> {
-
-    #[account(init, payer=user, space=9000)]
+#[instruction(bump: u8)]
+pub struct NewList<'info> {
+    #[account(init, payer=user, seeds=[b"gifListVikas2",  user.to_account_info().key.as_ref()], bump=bump, space=9000)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -38,18 +80,29 @@ pub struct StartStuffOff<'info> {
 
 #[derive(Accounts)]
 pub struct AddGif<'info> {
+    #[account(mut, seeds=[b"gifListVikas2", list_owner.to_account_info().key.as_ref()], bump=base_account.bump)]
+    pub base_account: Account<'info, BaseAccount>,
+    pub list_owner: UncheckedAccount<'info>,
+    pub user: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct UpvoteGif<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
+    pub user: Signer<'info>
 }
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct ItemStruct {
-    pub gif_link: String,
-    pub user_address: Pubkey
+pub struct GifItem {
+    pub url: String,
+    pub adder: Pubkey,
+    pub votes: u64
 }
 
 #[account]
 pub struct BaseAccount {
+    pub bump: u8,
+    pub gifs: Vec<GifItem>,
     pub total_gifs: u64,
-    pub gif_list: Vec<ItemStruct>,
 }
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,14 +1,87 @@
 const anchor = require('@project-serum/anchor');
-const { SystemProgram } = require('@solana/web3.js');
+const { SystemProgram, LAMPORTS_PER_SOL } = require('@solana/web3.js');
+const serumCmn = require('@project-serum/common');
 
 const main = async () => {
   console.log('🚀 Starting test...');
 
+  /*
+   * Provider to connect to Solana nodes.
+   */
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
+  console.log('🚀 Got and set provider...');
+
+  /*
+   * Program to interact with
+   */
   const program = anchor.workspace.Myepicproject;
+
+  console.log('🚀 Created program...');
+
+  /*
+   * Get baseAccount associated with this program?
+   */
+  async function getListAddress() {
+    console.log('Provider wallet publicKey: ', provider.wallet.publicKey);
+    console.log('program id: ', program.programId);
+    let [baseAccount, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from('gifListVikas2'), provider.wallet.publicKey.toBytes()],
+      program.programId
+    );
+
+    return { baseAccount, bump };
+  }
+
+  /*
+   * Create a new list under this wallet?
+   */
+  async function newList() {
+    let { baseAccount, bump } = await getListAddress();
+    console.log('Got baseAccount!');
+
+    let tx = await program.rpc.newList(bump, {
+      accounts: {
+        baseAccount,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+    });
+
+    console.log('Created a new list!');
+
+    let listData = await program.account.baseAccount.fetch(baseAccount);
+    return { baseAccount, listData };
+  }
+
+  // let { listData } = await newList();
+
+  let { baseAccount } = await getListAddress();
+  let listData = await program.account.baseAccount.fetch(baseAccount);
+  await program.rpc.addGif('Some Gif', {
+    accounts: {
+      baseAccount,
+      listOwner: provider.wallet.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  listData = await program.account.baseAccount.fetch(baseAccount);
+  console.log('List data after adding gif: ', listData);
+
+  await program.rpc.upvoteGif('Some Gif', {
+    accounts: {
+      baseAccount,
+      user: provider.wallet.publicKey,
+    },
+  });
+  listData = await program.account.baseAccount.fetch(baseAccount);
+  console.log('List data after upvoting gif: ', listData);
+
+  /*
   const baseAccount = anchor.web3.Keypair.generate();
+
+  console.log('🚀 Generated baseAccount...');
 
   let tx = await program.rpc.startStuffOff({
     accounts: {
@@ -36,7 +109,86 @@ const main = async () => {
   account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('👀 GIF Count', account.totalGifs.toString());
   console.log('👀 GIF list', account.gifList);
+
+  await program.rpc.upvoteGif(new anchor.BN(0), {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
+  console.log('👀 GIF list', account.gifList);
+
+  await program.rpc.sendSol(new anchor.BN(2), {
+    accounts: {
+      from: provider.wallet.publicKey,
+      to: baseAccount.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+  });
+
+  const fromAccount = await getTokenAccount(
+    provider,
+    provider.wallet.publicKey
+  );
+
+  let fromTokenAccount = await serumCmn.getTokenAccount(
+    program.provider,
+    fromAccount
+  );
+  console.log('From token account: ', fromTokenAccount);
+  */
 };
+
+async function getAccountBalance(pubkey) {
+  let account = await provider.connection.getAccountInfo(pubkey);
+  return account?.lamports ?? 0;
+}
+
+function expectBalance(actual, expected, message, slack = 20000) {
+  expect(actual, message).within(expected - slack, expected + slack);
+}
+
+async function createUser(airdropBalance) {
+  airdropBalance = airdropBalance ?? 10 * LAMPORTS_PER_SOL;
+  let user = anchor.web3.Keypair.generate();
+  let sig = await provider.connection.requestAirdrop(
+    user.publicKey,
+    airdropBalance
+  );
+  await provider.connection.confirmTransaction(sig);
+
+  let wallet = new anchor.Wallet(user);
+  let userProvider = new anchor.Provider(
+    provider.connection,
+    wallet,
+    provider.opts
+  );
+
+  return {
+    Key: user,
+    wallet,
+    provider: userProvider,
+  };
+}
+
+function createUsers(numUsers) {
+  let promises = [];
+  for (let i = 0; i < numUsers; i++) {
+    promises.push(createUser);
+  }
+
+  return Promise.all(promises);
+}
+
+function programForUser(user) {
+  return new anchor.Program(
+    mainProgram.idl,
+    mainProgram.programId,
+    user.provider
+  );
+}
 
 const runMain = async () => {
   try {
